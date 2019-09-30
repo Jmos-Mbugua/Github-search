89957057fc21a187b9cf01638f95b4b381a7d90d
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-09-30 14:24:23 +0300

    Adds Router module for landing page and   github search paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { UserProfileComponent } from './user-profile/user-profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileFormComponent } from './profile-form/profile-form.component'
 
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  {path: 'profile', component: UserProfileComponent},
+  {path: 'form', component: ProfileFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
